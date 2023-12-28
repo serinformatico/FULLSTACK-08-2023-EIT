250d7d06f5051b017e6ad85fb8f3db3712a81b82
Sergio Regalado Alessi <serdigital@live.com.ar>
2023-12-28 19:12:18 -0300

    Add Class N°41

---
+++ clase_41/mi_primer_proyecto/src/App.jsx
@@ -0,0 +1,10 @@
+// La siguiente línea de código, importa la hoja de estilos.
+import "./app.scss";
+
+const App = () => {
+    return (
+        <h1 className="title">Hola Mundo</h1>
+    );
+};
+
+export default App;
