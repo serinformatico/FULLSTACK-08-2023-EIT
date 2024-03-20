cd714d08b0ca34a881f5d7acda5cf7b9c7f9255e
Sergio Regalado Alessi <serdigital@live.com.ar>
2024-03-19 22:15:57 -0300

    Add Class N°60

---
+++ clase_60/backend/src/constants/messages.js
@@ -0,0 +1,13 @@
+const ERROR_ID_NOT_FOUND = "ID no encontrado";
+const ERROR_DATA_MISSING = "Faltan datos";
+const ERROR_SERVER = "Se ha generado un error en el servidor";
+const ERROR_UPLOAD_IMAGE = "Solo se permiten archivos PNG o JPG";
+const ERROR_UPLOAD_NULL = "Solicitud sin archivo";
+
+module.exports = {
+    ERROR_ID_NOT_FOUND,
+    ERROR_DATA_MISSING,
+    ERROR_SERVER,
+    ERROR_UPLOAD_IMAGE,
+    ERROR_UPLOAD_NULL,
+};
